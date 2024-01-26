2f2298e5153b7bfd692a22996447fc45442c44aa
jorjishasan <dev.jorjis@gmail.com>
2024-01-27 02:48:15 +0600

    add: functionality to render random image based on button click

--- Image Generator/src/components/RandomImage.js
+++ Image Generator/src/components/RandomImage.js
@@ -1,3 +1,32 @@
-const RandomImage = () => {};
+import { useEffect, useState } from "react";
+import { UNSPLASH_API, randomNum } from "../utils";
+
+const RandomImage = () => {
+  const [imgUrl, setImgUrl] = useState("");
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    getData();
+  }, [count]);
+
+  const getData = async () => {
+    const response = await fetch(UNSPLASH_API);
+    const data = await response.json();
+    setImgUrl(data[randomNum(0, 10)]?.urls?.small);
+  };
+
+  return (
+    <div className="container">
+      <div className="count">
+        <h1>Total Renders →</h1>
+        <h2>{count}</h2>
+        <button className="btn" onClick={() => setCount(count + 1)}>
+          Render ✦
+        </button>
+      </div>
+      <img src={imgUrl} />
+    </div>
+  );
+};
 
 export default RandomImage;
