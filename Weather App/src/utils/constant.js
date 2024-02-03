00724e891f231eb28b60a600f6aa08fa34ec21e1
jorjishasan <dev.jorjis@gmail.com>
2024-02-04 02:44:06 +0600

    add: api + api_key

---
+++ Weather App/src/utils/constant.js
@@ -0,0 +1,6 @@
+const API_KEY = "08373cd61534f123f9890109d221671b";
+const cityName = "Rajshahi";
+
+const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+
+export { WEATHER_API };
