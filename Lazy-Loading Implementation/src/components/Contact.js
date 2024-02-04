997b25c90fd5b5e379e99616b1bf85f899885eed
jorjishasan <dev.jorjis@gmail.com>
2024-02-05 00:17:55 +0600

    add: contact script

---
+++ Lazy-Loading Implementation/src/components/Contact.js
@@ -0,0 +1,12 @@
+import "../styles/Contact.scss";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1>Contact Page..</h1>
+    </div>
+  );
+  return <h1>Contact Page...</h1>;
+};
+
+export default Contact;
